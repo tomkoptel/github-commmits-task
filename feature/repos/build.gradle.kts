53c191a4befbba3722b3367c5e1845d64889328f
Tom Koptel <tom.koptel@gmail.com>
2021-08-08 11:56:53 +0200

    Fix project health

--- feature/repos/build.gradle.kts
+++ feature/repos/build.gradle.kts
@@ -16,11 +16,16 @@ android {
 }
 
 dependencies {
-    implementation(project(":retrofit-cache"))
+    api(project(":retrofit-cache"))
 
-    implementation(libs.bundles.network)
-    implementation(libs.kotlinx.coroutinesCore)
+    api(libs.okhttp3.core)
+    api(libs.retrofit2.core)
+    implementation(libs.retrofit2.gson)
+    implementation(libs.gson)
 
     testImplementation(testLibs.bundles.network)
-    testImplementation(testLibs.bundles.engine)
+    testImplementation(testLibs.junit4)
+    testImplementation(testLibs.kluent)
+    testImplementation(libs.kotlinx.coroutinesCore)
+    testImplementation(libs.kotlinx.coroutinesCoreJvm)
 }
