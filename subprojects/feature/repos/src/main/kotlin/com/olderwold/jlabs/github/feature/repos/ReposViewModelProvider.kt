d0f53a7d1487df2c6bfa41325cd5fab3b777bdb8
Tom Koptel <tom.koptel@gmail.com>
2021-08-08 14:59:31 +0200

    Suppress error

--- subprojects/feature/repos/src/main/kotlin/com/olderwold/jlabs/github/feature/repos/ReposViewModelProvider.kt
+++ subprojects/feature/repos/src/main/kotlin/com/olderwold/jlabs/github/feature/repos/ReposViewModelProvider.kt
@@ -2,6 +2,7 @@ package com.olderwold.jlabs.github.feature.repos
 
 import com.olderwold.jlabs.github.feature.repos.ui.ReposViewModel
 
+@Suppress("UnnecessaryAbstractClass")
 abstract class ReposViewModelProvider {
     internal abstract fun provide(): ReposViewModel
 }
