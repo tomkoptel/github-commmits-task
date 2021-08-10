2c76ad9df7fcdee2d1a7c3b3e7de59760fae70e2
Tom Koptel <tom.koptel@gmail.com>
2021-08-10 09:03:44 +0200

    Fix package name

--- subprojects/feature/details/src/main/kotlin/com/olderwold/jlabs/github/feature/details/data/dto/Committer.kt
+++ subprojects/feature/details/src/main/kotlin/com/olderwold/jlabs/github/feature/details/data/dto/Committer.kt
@@ -1,8 +1,8 @@
-package com.olderwold.jlabs.github.feature.data.dto
+package com.olderwold.jlabs.github.feature.details.data.dto
 
 import com.google.gson.annotations.SerializedName
 
-internal data class AuthorX(
+internal data class Committer(
     @SerializedName("date")
     val date: String? = null,
     @SerializedName("email")
