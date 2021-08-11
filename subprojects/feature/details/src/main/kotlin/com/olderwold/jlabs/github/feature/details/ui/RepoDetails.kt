ed7ee9967875d445aa4e90b8b116f0c67fe46eb5
Tom Koptel <tom.koptel@gmail.com>
2021-08-11 10:34:14 +0200

    Adjust preview for the RepoDetails.kt

--- subprojects/feature/details/src/main/kotlin/com/olderwold/jlabs/github/feature/details/ui/RepoDetails.kt
+++ subprojects/feature/details/src/main/kotlin/com/olderwold/jlabs/github/feature/details/ui/RepoDetails.kt
@@ -1,10 +1,13 @@
-package com.olderwold.jlabs.github.wip
+package com.olderwold.jlabs.github.feature.details.ui
 
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -14,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -25,10 +27,69 @@ data class MonthReport(
     val commitNumbers: Int,
     val month: String,
 ) {
+    val maxHeight: Dp = 80.dp
+    val maxWidth: Dp = 10.dp
+
     val barHeight: Dp
         get() {
-            return 50.dp * (commitNumbers.toFloat() / totalCommits.toFloat())
+            return maxHeight * (commitNumbers.toFloat() / totalCommits.toFloat())
         }
+}
+
+@Composable
+fun RepoDetails(items: List<MonthReport>) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        items(items) {
+            MonthCommits(it)
+        }
+    }
+}
+
+@Composable
+fun MonthCommits(
+    item: MonthReport,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
+        modifier = Modifier.height(IntrinsicSize.Min),
+    ) {
+        CommitBar(
+            barHeight = item.barHeight,
+            modifier = Modifier.requiredSize(
+                width = item.maxWidth,
+                height = item.maxHeight,
+            )
+        )
+        Text(
+            text = item.month,
+        )
+    }
+}
+
+@Composable
+fun CommitBar(
+    modifier: Modifier = Modifier,
+    barHeight: Dp,
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .background(Color.Gray),
+        )
+        Box(
+            modifier = Modifier.height(barHeight)
+                .fillMaxWidth()
+                .background(CommitColor),
+        )
+    }
 }
 
 @Composable
@@ -61,79 +122,14 @@ internal fun PreviewRepoDetails() {
 }
 
 @Composable
-fun RepoDetails(items: List<MonthReport>) {
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-        modifier = Modifier.requiredHeightIn(
-            max = 80.dp,
-        )
-    ) {
-        items(items) {
-            MonthCommits(it)
-        }
-    }
-}
-
-@Composable
-fun MonthCommits(
-    item: MonthReport,
-) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
-        modifier = Modifier.fillMaxHeight(),
-    ) {
-        CommitBar(
-            modifier = Modifier.requiredSize(
+@Preview
+fun CommitBarPreview() {
+    CommitBar(
+        barHeight = 20.dp,
+        modifier = Modifier
+            .requiredSize(
                 width = 10.dp,
-                height = item.barHeight,
+                height = 80.dp,
             )
-        )
-        Text(
-            text = item.month,
-        )
-    }
-}
-
-
-@Composable
-@Preview(
-    showBackground = true,
-    widthDp = 10,
-    heightDp = 30,
-)
-fun CommitBar(
-    modifier: Modifier = Modifier
-) {
-    Canvas(modifier = modifier) {
-        drawRect(
-            color = CommitColor,
-            size = size
-        )
-    }
-}
-
-internal class MonthReportProvider : PreviewParameterProvider<MonthReport> {
-    override val values: Sequence<MonthReport> = sequenceOf(
-        MonthReport(
-            totalCommits = 100,
-            commitNumbers = 90,
-            month = "April",
-        ),
-        MonthReport(
-            totalCommits = 100,
-            commitNumbers = 10,
-            month = "March",
-        ),
-        MonthReport(
-            totalCommits = 100,
-            commitNumbers = 10,
-            month = "May",
-        ),
-        MonthReport(
-            totalCommits = 100,
-            commitNumbers = 0,
-            month = "June",
-        ),
     )
 }
