7c602ad086dd67d3d76221528522e5f78edf9ac7
Tom Koptel <tom.koptel@gmail.com>
2021-08-30 13:41:10 +0200

    MainFragment: Add test to cover filtering

--- subprojects/app/src/test/kotlin/com/olderwold/jlabs/github/wrike/MainFragmentTest.kt
+++ subprojects/app/src/test/kotlin/com/olderwold/jlabs/github/wrike/MainFragmentTest.kt
@@ -1,14 +1,17 @@
 package com.olderwold.jlabs.github.wrike
 
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.MutableLiveData
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.adevinta.android.barista.interaction.BaristaEditTextInteractions.writeTo
 import com.adevinta.android.barista.interaction.BaristaListInteractions.scrollListToPosition
 import com.olderwold.jlabs.github.wrike.MainViewModel.Data
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -16,7 +19,7 @@ import org.junit.runner.RunWith
 class MainFragmentTest {
     private val liveData = MutableLiveData(emptyList<Data>())
 
-    private val viewModelMock = mockk<MainViewModel>() {
+    private val viewModelMock = mockk<MainViewModel>(relaxUnitFun = true) {
         every { dataLive } returns liveData
     }
 
@@ -25,13 +28,7 @@ class MainFragmentTest {
         liveData.value = listOf(
             Data(title = "Bilbo Baggins")
         )
-        val scenario = launchFragmentInContainer<MainFragment>(
-            factory = fragmentFactory {
-                overrideViewModel { viewModelMock }
-            }
-        )
-
-        scenario.moveToState(Lifecycle.State.RESUMED)
+        launch().moveToState(Lifecycle.State.RESUMED)
 
         assertDisplayed(android.R.id.edit)
         assertDisplayed(android.R.id.list)
@@ -39,4 +36,18 @@ class MainFragmentTest {
         scrollListToPosition(android.R.id.list, 0)
         assertDisplayed(android.R.id.text1, "Bilbo Baggins")
     }
+
+    @Test
+    fun `when user taps to edit text we perform filtering`() {
+        launch().moveToState(Lifecycle.State.RESUMED)
+
+        writeTo(android.R.id.edit, "You shall not pass")
+        verify { viewModelMock.filter(query = "You shall not pass") }
+    }
+
+    private fun launch() : FragmentScenario<MainFragment> = launchFragmentInContainer(
+        factory = fragmentFactory {
+            overrideViewModel { viewModelMock }
+        }
+    )
 }
